91f957b2a17656ac30403f58198bdb887ed1efd9
Remi Rampin <remi@rampin.org>
2021-06-18 12:36:23 -0400

    Other arguments

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 cd "$(dirname "$0")"
 musl-gcc -static \
+    -Os -s \
+    -fno-stack-protector -fomit-frame-pointer -ffunction-sections -fdata-sections -Wl,--gc-sections \
+    -fno-unwind-tables -fno-asynchronous-unwind-tables \
+    -fno-math-errno -fno-unroll-loops -fmerge-all-constants \
+    -fno-ident -Wl,-z,norelro \
     -W -Wall -Wextra -Werror -pedantic \
     rpzsudo.c \
     -o rpzsudo-$(uname -m)
